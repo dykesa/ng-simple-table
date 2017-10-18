729caa32b179de0dcb19211d7d336919c3b7c739
dykesa <dykesa@gmail.com>
2017-10-18 19:50:36 -0400

    Added FormsModule to imports to support ngModelChange events on form objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgSimpleTableComponent } from './ng-simple-table/ng-simple-table.component';
@@ -11,6 +12,7 @@ import { NgSimpleTableComponent } from './ng-simple-table/ng-simple-table.compon
   ],
   imports: [
     BrowserModule
+    , FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
